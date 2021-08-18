1f949a152fe5071bf817957f4805ce4865589150
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-18 10:21:08 +0200

    make global style

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/navbar";
+import  {GlobalStyle } from "./components/GlobalStyle"
 function App() {
   // const mood = true
   return (
     <div>
+      <GlobalStyle/>
       <Navbar />
 
     </div>
--- src/components/navbar/view.js
+++ src/components/navbar/view.js
@@ -1,9 +1,9 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const ContainerNav = styled.nav`
-width: 100vw;
-height: 88px;
-padding: 0px;
-margin: 0px
-
-`
+  width: 100vw;
+  height: 88px;
+  padding: 0px;
+  margin: 0px;
+  background-color: green;
+`;
