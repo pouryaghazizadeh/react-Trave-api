1fe75883cc683d09c892bcf7c99c2d00ffa565c4
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-18 10:29:57 +0200

    make toggel component

---
+++ src/components/Toggel/view.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+export const containerToggel = styled.div`
+  margin-right: 10px;
+  background-color: #2f3638;
+  width: 70px;
+  height: 30px;
+  border-radius: 44px;
+`;
