524d83615367948799f03e997e2e581ee73f5280
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-19 21:37:01 +0200

    fixed Toggel

--- src/components/Toggel/view.js
+++ src/components/Toggel/view.js
@@ -45,14 +45,15 @@ export const CheckBoxWrapper = styled.div`
 //   }
 // `;
 
-export const ToggelLabel = styled.label`
-  /* position: absolute;
+export const ToggleLabel = styled.label`
+  position: absolute;
   top: 0;
-  left: 0; */
-  width: 70px;
+  left: 0;
+  width: 55px;
   height: 30px;
   border-radius: 50px;
   background: #bebebe;
+
   cursor: pointer;
   &::after {
     content: "";
@@ -61,28 +62,24 @@ export const ToggelLabel = styled.label`
     width: 30px;
     height: 30px;
     /* margin: 3px; */
-    background: #ffffff;
-    transform: translateX(5px);
-    /* box-shadow: 1px 3px 3px 1px rgba(0, 0, 0, 0.2); */
-    /* transition: 0.2s; */
+    background: #fff;
+
+    box-shadow: 0.5px 0.5px 3px 2px rgba(0, 0, 0, 0.2);
+    transition: 0.2s;
   }
 `;
-export const ContainerToggel = styled.input`
+export const ContainerToggle = styled.input`
+
   opacity: 0;
-  /* z-index: 1; */
+  z-index: 1;
   border-radius: 44px;
-  width: 30px;
-  height: 30px;
-  &:checked + ${ToggelLabel} {
+  width: 25px;
+  height: 15px;
+  &:checked + ${ToggleLabel} {
     background: #4fbe79;
     &::after {
-      content: "";
-      display: block;
-      border-radius: 50px;
-      width: 30px;
-      height: 30px;
-      transform: translateX(-3px);
-      /* transition: 0.2s; */
+      transform: translateX(30px);
+      transition: 0.2s;
     }
   }
 `;
