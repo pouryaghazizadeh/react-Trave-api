f5103376b846af3a72ab049115ef37f436f6e74b
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-19 23:55:41 +0200

    bug toggle

--- src/components/Toggel/index.jsx
+++ src/components/Toggel/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import { CheckBoxWrapper, ContainerToggle, ToggleLabel } from "./view";
+import {useState}from "react"
 function Toggel() {
+const [r,setR] = usestate(false)
   return (
     <>
       <CheckBoxWrapper>
-        <ContainerToggle id="checkbox" type="checkbox" />
+        <ContainerToggle id="checkbox" type="checkbox" value = {r} onClick = {(e)=>{
+          setR(e.target.value) 
+          
+        }} />
         <ToggleLabel htmlFor="checkbox" />
       </CheckBoxWrapper>
+
+      <div style = {{
+background: {r?red:black}
+
+      }}>
+
+      </div>
     </>
   );
 }
