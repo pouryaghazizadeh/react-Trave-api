53f7782ba7d1581571b6d620f92033c424a47bc4
pourya ghazizadeh <pourya.gh095@gmail.com>
2021-08-27 12:36:58 +0200

    xeport components to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navbar from "./components/navbar";
 import  {GlobalStyle } from "./components/GlobalStyle"
+import {Switch,Route}from "react-router-dom"
+import Home from "./pages/Home/index"
+import Informations from "./pages/Informations/index"
+import PageNotFound from "./pages/PageNotFound/index"
 function App() {
   // const mood = true
   return (
     <div>
       <GlobalStyle/>
       <Navbar />
+      <Switch>
+        <Route exact path = "/"  component = {Home}/>
+<Route path = "/Informations" component = {Informations}/>
+<Route path = "/PageNotFound" component = {PageNotFound}
+      </Switch>
+    
 
     </div>
   );
